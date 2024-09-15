cffb090d192843dca66d8710738ed42e71fb517f
Juan Pablo Barrios <68880504+Juanpablobar@users.noreply.github.com>
2024-09-14 23:51:22 -0600

    fix: improving imports

--- src/tests/happyPath.spec.ts
+++ src/tests/happyPath.spec.ts
@@ -1,10 +1,10 @@
-import { home } from './home/home'
-import { pdp } from './pdp/pdp'
-import { cart } from './checkout/cart/cart'
-import { email } from './checkout/email/email'
-import { shipping } from './checkout/shipping/shipping'
-import { payment } from './checkout/payment/payment'
-import { orderplaced } from './checkout/orderplaced/orderplaced'
+import { home } from './home'
+import { pdp } from './pdp'
+import { cart } from './checkout/cart'
+import { email } from './checkout/email'
+import { shipping } from './checkout/shipping'
+import { payment } from './checkout/payment'
+import { orderplaced } from './checkout/orderplaced'
 import { getGifs } from '#utils/getGiphy'
 
 const executeHappyPath = async () => {
